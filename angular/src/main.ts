e7ca719aa99cda129618eca27774967c5ff2afd0
Wahaaj <wahaaj.ta@gmail.com>
2024-10-16 17:19:56 +0500

    Lab progress

---
+++ angular/src/main.ts
@@ -0,0 +1,20 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { LoginComponent } from './app/login/login.component';
+import { SignupComponent } from './app/signup/signup.component';
+import { ForgetPasswordComponent } from './app/forget-password/forget-password.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
